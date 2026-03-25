c2455855f39c2b20b47898c32623f0bc20aa152a
ahmed Emam <a.emam@elithair.com>
2026-03-25 22:48:09 +0200

    Add OTO sync scheduler - run oto:sync-shipments every 15 minutes

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Sync OTO shipment statuses every 15 minutes
+Schedule::command('oto:sync-shipments')->everyFifteenMinutes();
