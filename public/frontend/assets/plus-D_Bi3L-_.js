e4353c6c6a3e0c8e4e1a68c70ddf98e90c3d165e
ahmed Emam <a.emam@elithair.com>
2026-04-14 00:43:19 +0200

    Remove dark mode icon from mobile navbar

--- public/frontend/assets/plus-D_Bi3L-_.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./index-D6l_8DQw.js";/**
- * @license lucide-react v0.534.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],t=e("plus",o);export{t as P};
