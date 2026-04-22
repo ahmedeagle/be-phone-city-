626932b2e77c333c30b398b6192c5d13513f8179
ahmed Emam <a.emam@elithair.com>
2026-04-22 20:18:40 +0200

    style(footer): solid navy bg matching banner; remove brand title and about text

---
+++ public/assets/send-B4ZqeSd_.js
@@ -0,0 +1,11 @@
+import{j as a}from"./index-BvzVKRRI.js";/**
+ * @license lucide-react v0.534.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z",key:"1lielz"}]],c=a("message-square",e);/**
+ * @license lucide-react v0.534.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],t=a("send",s);export{c as M,t as S};
