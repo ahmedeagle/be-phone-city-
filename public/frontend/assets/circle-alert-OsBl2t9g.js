b6595d359b9c604b9a8eaa02841787b0fd5af331
ahmed Emam <a.emam@elithair.com>
2026-04-27 23:27:19 +0300

    fix: rewrite offers layout with pure inline styles + fix madfu visibility in footer

---
+++ public/frontend/assets/circle-alert-OsBl2t9g.js
@@ -0,0 +1,11 @@
+import{j as e}from"./index-CkDOLHcj.js";/**
+ * @license lucide-react v0.534.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=e("chevron-up",c);/**
+ * @license lucide-react v0.534.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],t=e("circle-alert",o);export{n as C,t as a};
